7bd7b886cda892c59b4b9fad8ab008d141d0ef16
dltngud1541 <dltngud110@gmail.com>
2024-04-22 17:58:55 +0900

    feat: Creating def: random number boxes
    실행 시, 1~100까지의 숫자들이 랜덤 배정된  리스트 값을 반환하는 함수 생성

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Welcome second project")
+import random
+
+#중복 없느 1~100 숫자가 들어간 랜덤 박스 리스트 생성
+def random_boxes():
+    # 1부터 100까지의 숫자로 이루어진 리스트 생성
+    numbers =list(range(1,101))
+
+    # numbers 리스트에서 중복 없이 100개의 숫자를 랜덤배치하는 새로운 리스트 생성
+    random_numbers = random.sample(numbers, 100)
+
+    return random_numbers
+
+
+print(random_boxes())
